d23c6d0f5f805c78edaccf7a879ad392d29e9d6b
BokaMokoena <102230320+BokaMokoena@users.noreply.github.com>
2024-05-18 00:28:01 +0200

    Update Helper.java

--- CarShare application/src/main/java/za/ac/cput/util/Helper.java
+++ CarShare application/src/main/java/za/ac/cput/util/Helper.java
@@ -73,5 +73,15 @@ public class Helper {
                 isValidSuburb(address.getSuburb()) &&
                 isValidPostalCode(address.getPostalCode());
     }
+      public static boolean isNullOrEmpty(String s) {
+        if (s == null || s.isEmpty())
+            return true;
+        return false;
+
+    }
+      public static String generateId(){
+        return UUID.randomUUID().toString();
+
+    }
 }
 
