72a2952be12a800c367dbb7c6eec8af01766036b
Kabo Khudunyane <88645384+KaboKhudunyane@users.noreply.github.com>
2024-05-19 19:59:29 +0200

    Update Name.java
    Remember to add copy

--- CarShare application/src/main/java/za/ac/cput/domain/Name.java
+++ CarShare application/src/main/java/za/ac/cput/domain/Name.java
@@ -1,34 +1,37 @@
 package za.ac.cput.domain;
-import jakarta.persistence.Embeddable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
+
 @Entity
 public class Name implements Serializable {
     @Id
     private String firstName;
     private String middleName;
     private String lastName;
+
     protected Name() {}
-    public Name(String firstName, String middleName, String lastName) {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
+
+    public Name(Builder builder) {
+        this.firstName = builder.firstName;
+        this.middleName = builder.middleName;
+        this.lastName = builder.lastName;
     }
-    public Name(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+
     public String getFirstName() {
         return firstName;
     }
+
     public String getMiddleName() {
         return middleName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     @Override
     public boolean equals(Object nameObject) {
         if (this == nameObject) return true;
@@ -38,10 +41,12 @@ public class Name implements Serializable {
                 Objects.equals(middleName, name.middleName) &&
                 Objects.equals(lastName, name.lastName);
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(firstName, middleName, lastName);
     }
+
     @Override
     public String toString() {
         return "Name{" +
@@ -49,5 +54,30 @@ public class Name implements Serializable {
                 ", middleName='" + middleName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 '}';
+    }
+
+    public static class Builder {
+        private String firstName;
+        private String middleName;
+        private String lastName;
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setMiddleName(String middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Name buildName() {
+            return new Name(this);
+        }
     }
 }
