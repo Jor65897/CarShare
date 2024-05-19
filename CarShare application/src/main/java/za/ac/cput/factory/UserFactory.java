7ddd595ffe45e003d88d633eb321a3087ac974ab
Kabo Khudunyane <88645384+KaboKhudunyane@users.noreply.github.com>
2024-05-19 20:33:53 +0200

    Update UserFactory.java

--- CarShare application/src/main/java/za/ac/cput/factory/UserFactory.java
+++ CarShare application/src/main/java/za/ac/cput/factory/UserFactory.java
@@ -1,13 +1,26 @@
-package za.ac.cput.factory;
-
+package za.ac.cput.factory; 
 import za.ac.cput.domain.User;
 import za.ac.cput.domain.Name;
 import za.ac.cput.domain.Contact;
 import za.ac.cput.domain.Address;
-
+import za.ac.cput.util.Helper; 
 public class UserFactory {
 
-    public static User createUser(String userID, Name name, Contact contact, Address address, Boolean license, String role, String picture, Boolean verified) {
+    public static User createUser(String userID, Name name,
+                                  Contact contact, Address address,
+                                  Boolean license, String role,
+                                  String picture, Boolean verified) {
+
+        if(Helper.isNullOrEmpty(userID) ||
+        Helper.isNullOrEmpty(name) ||
+        Helper.isNullOrEmpty(contact) ||
+        Helper.isNullOrEmpty(address) ||
+        Helper.isNullOrEmpty(license) ||
+        Helper.isNullOrEmpty(role) ||
+        Helper.isNullOrEmpty(picture) ||
+        Helper.isNullOrEmpty(verified))
+            return null;
+
         return new User.Builder()
                 .setUserID(userID)
                 .setName(name)
@@ -17,12 +30,7 @@ public class UserFactory {
                 .setRole(role)
                 .setPicture(picture)
                 .setVerified(verified)
-                .build();
+                .buildUser();
     }
 
-    public static User copyUser(User user) {
-        return new User.Builder()
-                .copy(user)
-                .build();
-    }
 }
